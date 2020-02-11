46ce9511e9df037eba4b171b55dfc207601ebb95
Lidii <lidiiany92@gmail.com>
2020-02-11 09:58:14 -0800

    minor changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //READ/WRITE FILES
 ///////////////////////////
@@ -20,29 +22,14 @@ const url = require('url');
 
 //SERVER
 
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%EMOJI%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%NUTRITIENTS%}/g, product.nutrients);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-
-    return output;
-}
-
 // ** readFileSunc called only once in the beginning of executing index.js file **
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(item => slugify(item.productName, {lower: true}));
 
 // **this function is called everytime there's a new request **
 const server = http.createServer((req, res) => {
---
+++ modules/replaceTemplate.js
@@ -0,0 +1,18 @@
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%EMOJI%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%NUTRITIENTS%}/g, product.nutrients);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+
+    return output;
+}
+
+module.exports = replaceTemplate;
